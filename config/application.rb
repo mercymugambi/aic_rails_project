7283b90c1b99d866978396bcb8aaf9ddbb550415
Mercy Mugambi <mercymugambi2016@gmail.com>
2026-07-19 20:59:28 +0300

    Commit Nav pages

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,17 @@ Bundler.require(*Rails.groups)
 
 module AicRailsProject
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+        # CORS settings
+        config.middleware.insert_before 0, Rack::Cors do
+          allow do
+            origins '*' # or specify your React app's origin# The origin of your frontend app
+            resource '*',
+              headers: :any,
+              methods: [:get, :post, :put, :patch, :delete, :options, :head],
+              credentials: false
   end
+end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :home, only: [:index]
   namespace :api do
     namespace :v1 do
+      post '/payment', to: 'payments#create'  # Updated this line
+      post '/callback', to: 'payments#callback'
       
       resources :users, only: [:index]
       resources :members
