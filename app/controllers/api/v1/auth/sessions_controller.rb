a526b5a6126f6c2750845b9a7768fcbd503d09e7
Mercy Mugambi <mercymugambi2016@gmail.com>
2026-08-11 10:55:00 +0300

    Update sessions controller response attributes and environment settings

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -5,6 +5,7 @@ module Api
       class SessionsController < Devise::SessionsController
         respond_to :json
         skip_before_action :verify_authenticity_token
+        skip_before_action :require_no_authentication, raise: false
 
         private
 
